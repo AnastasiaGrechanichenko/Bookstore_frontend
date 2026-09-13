a31af2ee7239f22c0ba98e78f994d1e20ad16a24
Anastasia Grechanichenko <anastasiagrecanicenko204@gmail.com>
2026-09-13 19:30:17 +0300

    Dockerfile,client.js

--- src/api/client.js
+++ src/api/client.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8000"
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000"
 
 export const apiFetch = async (path,options = {}) => {
     const secret = localStorage.getItem('session-secret')
